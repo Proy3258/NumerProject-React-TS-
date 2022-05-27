3c0484022677a8673ea768d459745927307f5709
proy3258 <proy3258@gmail.com>
2022-05-27 10:24:50 +0700

    [Update] CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-import  Bisection from './rootofequations/Bisection'
-
-
 
 test('Test Home Page', () => {
   render(<App/>);
-  const linkElement = screen.getByText(/Numer Project React TS/i);
+  const linkElement = screen.getByText(/ROOT OF EQUATION/i);
   expect(linkElement).toBeInTheDocument();
   fireEvent.click(screen.getByText(/ROOT OF EQUATION/i));
 });
 
 // test('Test Bisection', ()=>{
-//   render(<Bisection/>);
-//   const linkElement = expect(component).toHaveTextContent(/BISECTION/i);
+//   let component = /Bisection/i;
+//   const linkElement = screen.getByText(/Numer Project React TS/i);
 //   // fireEvent.click(screen.getByText(/GET STARTED/i));
 
 // });
